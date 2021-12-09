fa2135aa0b764a1bbc5c4d1072ec9c3a1bd7db3a
Denis Venher <87716979+VenjaVenja@users.noreply.github.com>
2021-12-09 12:38:16 +0200

    add_gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const imagesMarkup = galleryItems
+.map(({preview,original,description}) => 
+`<div class = "gallery__item">
+<a class="gallery__link" href="${original}">
+<img
+  class="gallery__image"
+  src="${preview}"
+  data-source="${original}"
+  alt="${description}"
+/>
+</a></div>`)
+.join("");
+
+
+galleryEl.insertAdjacentHTML('afterbegin',imagesMarkup);
+
+galleryEl.addEventListener('click', onImageClick)
+
+function onImageClick (event){
+    event.preventDefault();
+
+const isImage = event.target.classList.contains('gallery__image');
+    if (!isImage) {
+    return
+};
+
+const modalWindow = basicLightbox.create(
+    `<div class='modal'>
+ <img src='${event.target.dataset.source}' width='800' height='600'>
+</div>`);
+
+modalWindow.show()
+
+document.addEventListener('keydown', onKeyboardEscClick);
+
+function onKeyboardEscClick (event) {
+if(event.code === 'Escape'){
+    modalWindow.close();
+    document.removeEventListener('keydown', onKeyboardEscClick)
+}
+}
+};
